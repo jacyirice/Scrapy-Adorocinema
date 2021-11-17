20c0799567a8caddbec1ae0c29cfcc0e70b04a68
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-11-17 00:01:37 -0300

    Adicionando extração de dados para todas as paginas

--- adorocinema/adorocinema/spiders/adorocinema_spyder.py
+++ adorocinema/adorocinema/spiders/adorocinema_spyder.py
@@ -6,12 +6,18 @@ class ImdbSpyder(scrapy.Spider):
     name = 'adorocinema'
     start_urls = [
         'https://www.adorocinema.com/filmes/melhores/']
-    
-    page=1
+
+    page = 1
+
     def parse(self, response):
         response_string = response.text
         films = re.findall(">[0-9],[0-9]<", response_string)
         yield {
-                'total_films': len(films),
-                'all_notes': films,
+            'total_films': len(films),
+            'all_notes': films,
         }
+
+        next_page = re.findall("Próxima", response_string)
+        if next_page is not None:
+            self.page += 1
+            yield response.follow(f'{response.url.split("?")[0]}?page={self.page}', callback=self.parse)
